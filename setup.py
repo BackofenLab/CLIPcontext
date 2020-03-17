0744efe7d03975e336449d7f2ff640d9cbdfb6a5
Michael Uhl <uhl.michael@gmx.de>
2020-03-17 14:48:58 +0100

    mv to bin + add setup.py

---
+++ setup.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+
+from setuptools import setup
+
+
+"""Setup CLIPcontext"""
+
+setup(
+    name='clipcontext',
+    version='0.1',
+    description='Extract CLIP-seq binding regions with both genomic and transcript context',
+    long_description=open('README.md').read(),
+    author='Michael Uhl',
+    author_email='uhlm@informatik.uni-freiburg.de',
+    url='https://github.com/BackofenLab/CLIPcontext',
+    license='MIT',
+    scripts=['bin/clipcontext'],
+    packages=['clipcontext'],
+    include_package_data=True,
+)
+
